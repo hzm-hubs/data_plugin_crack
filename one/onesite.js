d3838cd6cbe1f4a225e0d6209c995a08322631ad
hzm <467251648@qq.com>
2025-06-03 23:13:36 +0800

    测试获取展现量数据

--- one/onesite.js
+++ one/onesite.js
@@ -16,24 +16,28 @@ let lastTableContent = "";
 async function getPageData(tableList) {
   // 前一版本 asiYysdGuS （常规） asiYysdGuW（选中）
   // 现在版本 asiYyskwuM         asiYyskwuQ
-  const nodeSelectors = document.querySelectorAll("[mxv=pageSizes]");
-  if (!nodeSelectors.length) {
+  const nodeSelector = document.querySelector("[mxv=pageSizes]");
+  if (!nodeSelector) {
     return;
   }
-  const pagesNode = nodeSelectors[0].getElementsByTagName("a");
+  // document.querySelectorAll('a[mx-click*="magix-portses"]')
+  const pagesNode = nodeSelector.getElementsByTagName("a");
   let maxClassValue = 0;
   let curPageNum = 0;
-  Array.from(pagesNode).forEach((it, index) => {
-    // 根据css样式长度，找到当前选中的页码
-    if (it.classList.length > maxClassValue) {
-      maxClassValue = it.classList.length;
-      curPageNum = it.textContent;
-    }
-  });
+  Array.from(pagesNode)
+    .filter((it) => !isNaN(it.textContent))
+    .forEach((it, index) => {
+      // 根据css样式长度，找到当前选中的页码
+      if (it.classList.length > maxClassValue) {
+        maxClassValue = it.classList.length;
+        curPageNum = it.textContent;
+      }
+    });
   console.log("当前选中页码", curPageNum);
   if (curPageNum == 1) {
     getCurrentTables(tableList);
   }
+  return;
   for (let i = curPageNum == 1 ? 1 : 0; i < pagesNode.length; i++) {
     chrome.runtime.sendMessage({
       from: "myseller",
@@ -108,11 +112,63 @@ const fieldsList = [
   "boostingDrivingClick",
 ];
 
+// 获取剪贴板文本内容
+async function getClipboardText() {
+  try {
+    const text = await navigator.clipboard.readText();
+    console.log("剪贴板内容:", text);
+    return text;
+  } catch (err) {
+    console.error("无法读取剪贴板:", err);
+    return "";
+  }
+}
+
+function readItDetail() {
+  return new Promise((resolve) => {
+    const result = [];
+    const dialogBody = document.querySelector(".dialog-modal-body");
+    dialogBody.getElementsByTagName("a")[2].click();
+    dialogBody
+      .querySelectorAll('[mxv="detailMode"]')[1]
+      .querySelectorAll("[mx-click*=magix-portsu]")[1]
+      .click(); // 切换显示模式 到 表格
+    const targetTable = dialogBody
+      .querySelectorAll('div[mxv*="listPrefix,fields"]')[0]
+      .querySelectorAll("table");
+    const theads = Array.from(
+      targetTable[0].querySelector("thead").getElementsByTagName("th")
+    ).map((it) => it.textContent);
+    Array.from(
+      targetTable[1].querySelector("tbody").getElementsByTagName("tr")
+    ).forEach((item) => {
+      const itemObj = {};
+      const valueList = item.innerText.split("\t");
+      valueList.forEach((it, index) => {
+        if (!it.includes("\n")) {
+          tempObj[tableHeads[index]] = it;
+        } else {
+          tempObj[tableHeads[index]] = it.split("\n").filter((value) => value);
+        }
+      });
+    });
+    resolve(result);
+  });
+}
+
 function getCurrentTables(result) {
   lastTableContent = JSON.stringify(getTargetTable("tbody")?.innerHTML);
-  Array.from(getTargetTable("tbody").getElementsByTagName("tr")).forEach(
-    (it, index) => {
-      // 排除子项下方操作项，并且不包含合计项
+  Array.from(
+    getTargetTable("tbody").getElementsByTagName("tr").slice(0)
+  ).forEach(async (it, index) => {
+    // 排除子项下方操作项，并且不包含合计项
+    console.log("操作栏1", it.getAttribute("mx-stickytable-operation"));
+    if (it.getAttribute("mx-stickytable-operation")) {
+      it.querySelector('button[mxv="popData"]').click();
+      const data = await readItDetail();
+      console.log("操作栏2", data);
+    } else {
+      console.log("数据项");
       if (
         Array.from(it.children).length > 2 &&
         !it.childNodes[0].textContent.includes("合计")
@@ -136,26 +192,26 @@ function getCurrentTables(result) {
         result.push(tempObj);
       }
     }
-  );
+  });
 }
 
 async function scrapeOneData(callBack) {
   try {
     // 获取表头
-    // const targetHead = getTargetTable();
-    // if (!targetHead) {
-    //   throw "no table data";
-    // }
-    // targetHead.scrollIntoView();
-    // tableHeads = Array.from(
-    //   targetHead.getElementsByTagName("thead")?.[0].getElementsByTagName("th")
-    // ).map((it) => it.innerText.split("\n")[0]);
-    // tableHeads.splice(2, 0, "详情页");
-    // tableHeads.splice(2, 0, "商品图片");
-    // // 设置表数据
-    // const tableList = [];
-    // await getPageData(tableList);
-    // return tableList;
+    const targetHead = getTargetTable();
+    if (!targetHead) {
+      throw "no table data";
+    }
+    targetHead.scrollIntoView();
+    tableHeads = Array.from(
+      targetHead.getElementsByTagName("thead")?.[0].getElementsByTagName("th")
+    ).map((it) => it.innerText.split("\n")[0]);
+    tableHeads.splice(2, 0, "详情页");
+    tableHeads.splice(2, 0, "商品图片");
+    // 设置表数据
+    const tableList = [];
+    await getPageData(tableList);
+    return tableList;
   } catch (e) {
     console.log("uploadFile error: ", e);
     return [];
